40b7478a350189f782cda361373c93d083a0bbc5
nerfsunny <nerfsunny@gmail.com>
2022-05-28 18:27:13 -0400

    Updated download_data.py
    Renamed the method downloadMyAnimeData to myAnimeData. Added parameters such that the program calling it can store the data in the correct place.
    
    Created the method hofAnimeData. This uses scrapes a given challenge page for eligible entries for that specific challenge. It then compiles the information in a dictionary, which it then returns.

--- helper/download_data.py
+++ helper/download_data.py
@@ -1,23 +1,51 @@
-import os
 import requests
-from dotenv import load_dotenv
 import json
+from bs4 import BeautifulSoup as bs
+from helper.custom_classes import hofAnime
 
-def downloadMyAnimeData():
+def myAnimeData(malClientID, myAnimeList, selectedFields, directoryPath):
     animeListStatus = ['watching', 'completed', 'dropped', 'on_hold', 'plan_to_watch']
     selectedLimit = '1000'
     print("Downloading my anime data.")
     for myStatus in animeListStatus:
-        with open('../MAL Anime Data/' + myStatus + '.json', 'w', encoding='utf-8') as file:
+        with open(directoryPath + myStatus + '.json', 'w', encoding='utf-8') as file:
             data = requests.get(myAnimeList, headers={'X-MAL-CLIENT-ID' : malClientID}, params={'status' : myStatus, 'fields' : selectedFields, 'nsfw' : 'true', 'limit' : selectedLimit})
             json.dump(data.json(), file, ensure_ascii=False, indent=4)
             print('\t' + myStatus + " complete...")
 
-load_dotenv()
+def hofAnimeData(themes_tags):
+    hof = dict()
 
-malClientID = os.getenv('X-MAL-CLIENT-ID')
-myAnimeList = os.getenv('myAnimeList')
-selectedFields = 'rating, media_type, genres, studios, list_status'
+    for theme in themes_tags:
+        link = f'https://anime.jhiday.net/hof/challenge/{theme}#challengeItems'
+        request = requests.get(link)
 
-downloadMyAnimeData()
-print("Finished downloading data.")
+        if request.status_code != 200:
+            print(f'Error connecting to {theme} on HoF')
+            continue
+
+        print(f'Connected successfully to {theme} on HoF')
+        pageSoup = bs(request.content, "html.parser")
+
+        print('\tGathering data.')
+        contentTab = pageSoup.find_all('a', {'href' : '#challengeItems'})
+        eligibleItems = pageSoup.find_all('li', {'category' : 'All Series'})
+
+        numberOfExpectedItems = int(contentTab[0].text.strip().split('\n')[1])
+
+        if numberOfExpectedItems != len(eligibleItems):
+            print("Number of items obtained does not match expected size. Passing.")
+            continue
+
+        print('\tProcessing data.')
+        for item in eligibleItems:
+            malID = item.find('a', {'class' : 'seriesLink'})['seriesid']
+
+            if malID not in hof.keys():
+                hof[malID] = hofAnime(malID, item.text.strip())
+
+            hof[malID].addTheme(theme)
+
+        print('\tDone')
+    
+    return hof
