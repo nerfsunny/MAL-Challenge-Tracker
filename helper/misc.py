c4d930fb1ab7240e8cbce99cfa32991f7d60d671
nerfsunny <nerfsunny@gmail.com>
2022-05-28 18:20:29 -0400

    Created canUpdateData method in misc.py.

--- helper/misc.py
+++ helper/misc.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+from dotenv import set_key
+import os
 
 def storeAnimeUsed(challengeName, runNumber, series):
-    with open('./export_challenges/studios/test.txt', 'w') as file:
-        file.write("Hello World 2")
+    with open(f'./export_challenges/{challengeName}/{challengeName}_{runNumber}_animeUsed.txt', 'w') as file:
+        for id in series:
+            file.write(f'{id}\n')
+
+def canUpdateData(envFile, envVariable, date_format, incNumberOfWeeks):
+    importDate = dt.datetime.strptime(os.getenv(envVariable), date_format).date()
+    currentDate = dt.date.today()
+
+    if currentDate > importDate:
+        print(f'Can update data. Incrementing import date by {incNumberOfWeeks} weeks.')
+        set_key(envFile, envVariable, (dt.date.today() + relativedelta(weeks=+incNumberOfWeeks)).strftime(date_format))
+        return True
+
+    print(f'Cannot update data. Next scheduled update is {importDate}')
+    return False
